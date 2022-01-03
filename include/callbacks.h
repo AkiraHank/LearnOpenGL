2e4c4d3e2323621782858d2b2f6794ed1a7e435d
AkiraHank <hanktan46@163.com>
2022-01-03 20:11:20 +0800

    init

---
+++ include/callbacks.h
@@ -0,0 +1,19 @@
+//
+// Created by 谭浩鸣 on 2021/11/30.
+//
+
+#ifndef OPENGL_TEST_CALLBACKS_H
+#define OPENGL_TEST_CALLBACKS_H
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+
+//non-callbacks
+void processInput(GLFWwindow *window);
+
+
+//static data
+extern const char *vertexShaderSource;
+extern const char *fragmentShaderSource;
+
+#endif //OPENGL_TEST_CALLBACKS_H
---
+++ src/callbacks.cc
@@ -0,0 +1,30 @@
+//
+// Created by 谭浩鸣 on 2021/11/30.
+//
+#include "callbacks.h"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+//static data
+const char *vertexShaderSource = "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}\n\0";
+ 
